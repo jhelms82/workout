fd4b19a17e3ce4beba5387116ac24d5370f0da27
James Helms <jhelms1014@gmail.com>
2021-10-12 10:48:42 -0500

    service worker added

---
+++ js/app.js
@@ -0,0 +1,17 @@
+//does the browser support service worker?
+
+if('serviceWorker' in navigator){
+//then register out service worker
+navigator.serviceWorker.register('/sw.js').then(reg => {
+    //display a success message
+    console.log('Service Worker Registration (Scope: ${reg.scope})');
+})
+    .catch(error => {
+    //display error message
+    console.log('Service Worker error (${error}');
+});
+}else{
+    //happens when the app isnt servered over a TLS connection(HTTP)
+    //or if the broswer doesnt support the service worker
+    console.log("Service Worker not available")
+}
