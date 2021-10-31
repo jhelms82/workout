c0db48a6d32c12ff0b79be863f7c8a8c045a3a9c
James Helms <jhelms1014@gmail.com>
2021-10-31 12:56:43 -0500

    fixed

--- js/app.js
+++ js/app.js
@@ -1,19 +1,36 @@
-//does the browser support service worker?
+// //does the browser support service worker?
 
-if("serviceWorker" in navigator){
-//then register out service worker
-navigator.serviceWorker
-.register("sw.js")
-.then((reg => {
-    //display a success message
-    console.log('Service Worker Registration (Scope: ${reg.scope})');
-})
-    .catch((error) => {
-    //display error message
-    console.log('Service Worker error (${error})');
-    });
-} else {
-    //happens when the app isnt servered over a TLS connection(HTTP)
-    //or if the broswer doesnt support the service worker
-    console.log("Service Worker not available");
+// if ("serviceWorker" in navigator){
+//     //defer sw install until page completes loading
+//     window.addEventListener("load", () => {
+
+// //then register out service worker
+// navigator.serviceWorker
+// .register("sw.js")
+// .then((reg) => {
+//     //display a success message
+//     console.log(`Service Worker Registration (Scope: ${reg.scope})`);
+// })
+//     .catch((error) => {
+//     //display error message
+//     console.log(`Service Worker error (${error})`);
+//     });
+// });
+    
+// } else {
+//     //happens when the app isnt servered over a TLS connection(HTTP)
+//     //or if the broswer doesnt support the service worker
+//     console.log("Service Worker not available");
+//     }
+
+if ('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/sw.js').then(function(registration){
+    console.log('service worker registration succeeded:',registration);
+  },
+function(error){
+  console.log('service worker registration failed:',error);
+});
+}
+else{
+  console.log('service workers are not supported.');
 }
--- sw.js
+++ sw.js
@@ -1,22 +1,42 @@
+// const assets = [
+//     '/',
+//     "/index.html",
+//     "/js/app.js",
+//     "/js/materialize.min.js",
+//     "/css/style.css",
+//     "/css/materialize.min.css",
+//     "/img/workout.jpg"
+// ]
+
+
 self.addEventListener('install', function(event){
     //fires when the browser install the app
     //here we just logging the enet and the contents of the object passed to the event
-    //purpose of this is to give the service worker a plae to setup the local
+    //purpose of this is to give the service worker a plae to secactup the local
     //environment after the installation completes
-    console.log('SW: Event fire: ${event.type}');
-})
+    console.log(`SW: Event fire: ${event.type}`);
+    // event.waitUntil(caches.open('static').then(function(cache){
+    //     console.log("SW: Precaching App shell");
+    //     cache.addAll(assets);
+    // })
+    // );
+});
 
 self.addEventListener('activate', function(event){
     //fires when the browser install the app
   //its a place for the service worker to clean up from
   //previous service worker versions
 
-    console.log('SW: Event fire: ${event.type}');
-})
+    console.log(`SW: Event fire: ${event.type}`);
+});
 
 self.addEventListener('fetch', function(event){
     //fires whenever the app request a resource (file or data)
-    console.log("SW: fetching ${event.request.url}");
+    console.log(`SW: fetching ${event.request.url}`);
     //next, go get the requested resource from the network
     event.respondWith(fetch(event.request));
-})
+    // caches.match(event.request).then(function(response){
+    //     return(response) || fetch(event.request);
+    // });
+
+});
